fb975004f1d3b660f45d3c49e9a7c20b3ddeeebe
MattheDev53 <matthew.c.albrecht@gmail.com>
2024-10-13 11:23:48 -0500

    Fix Sick Ass Install Script

--- installGSS.sh
+++ installGSS.sh
@@ -6,14 +6,8 @@
 echo "Cloning repo"
 git clone https://github.com/MattheDev53/GoofyShellScripts.git
 
-# Symlink the repo folder to a central directory that's easy to reach
-if [ ! -d ~/.progs ]; then
-    echo "Creating ~/.progs"
-    mkdir ~/.progs
-fi
-    ln -s ~/GoofyShellScripts ~/.progs
-
 # Edit .bashrc
 echo "Editing .bashrc"
-echo "alias startShizuku=\"~/.progs/startShizuku.sh\"" >> ~/.bashrc
-echo "alias tmuxClear=\"~/.progs/tmuxClear.sh\"" >> ~/.bashrc
+echo "alias startShizuku=\"~/GoofyShellScripts/startShizuku.sh\"" >> ~/.bashrc
+echo "alias tmuxClear=\"~/GoofyShellScripts/tmuxClear.sh\"" >> ~/.bashrc
+echo "Done!"
