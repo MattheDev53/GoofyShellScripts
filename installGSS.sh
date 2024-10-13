a5b029e98c78d59b069190238edc048f896cf41c
MattheDev53 <matthew.c.albrecht@gmail.com>
2024-10-13 11:28:06 -0500

    More fixes

--- installGSS.sh
+++ installGSS.sh
@@ -8,6 +8,6 @@ git clone https://github.com/MattheDev53/GoofyShellScripts.git
 
 # Edit .bashrc
 echo "Editing .bashrc"
-echo "alias startShizuku=\"~/GoofyShellScripts/startShizuku.sh\"" >> ~/.bashrc
-echo "alias tmuxClear=\"~/GoofyShellScripts/tmuxClear.sh\"" >> ~/.bashrc
+echo "alias startShizuku='bash ~/GoofyShellScripts/startShizuku.sh'" >> ~/.bashrc
+echo "alias tmuxClear='bash ~/GoofyShellScripts/tmuxClear.sh'" >> ~/.bashrc
 echo "Done!"
